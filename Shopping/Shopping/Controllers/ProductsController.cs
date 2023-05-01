b06c2e7e51d4162f76da3b014e90d398940c3518
danilolucena <daniloj.lucena@gmail.com>
2023-05-01 20:10:39 -0300

    Fin Murio en producto

--- Shopping/Shopping/Controllers/ProductsController.cs
+++ Shopping/Shopping/Controllers/ProductsController.cs
@@ -95,5 +95,84 @@ namespace Shopping.Controllers
             model.Categories = await _combosHelper.GetComboCategoriesAsync();
             return View(model);
         }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Product producto = await _context.Products.FindAsync(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            EditProductViewModel model = new()
+            {
+                Description = producto.Description,
+                Id = producto.Id,
+                Name = producto.Name,
+                Price = producto.Price,
+                Stock = producto.Stock
+            };
+
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, CreateProductViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+            try
+            {
+                Product product = await _context.Products.FindAsync(model.Id);
+                product.Description = model.Description;
+                product.Name = model.Name;
+                product.Price = model.Price;
+                product.Stock = model.Stock;
+                _context.Update(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                {
+                    ModelState.AddModelError(string.Empty, "Ya existe un producto con el mismo nombre.");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                ModelState.AddModelError(string.Empty, exception.Message);
+            }
+            return View(model);
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Product product = await _context.Products
+            .Include(p => p.ProductImages)
+            .Include(c => c.ProductCategories)
+            .ThenInclude(pc => pc.Category)
+            .FirstOrDefaultAsync(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+
+        }
+
     }
+
+
 }
